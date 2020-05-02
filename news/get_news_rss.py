7ff9d13f86cc69b6b0d346eee714b31a4d9077be
Julian Dragoi <julian_dragoi@hotmail.co.uk>
2020-05-02 15:19:18 +0100

    made get_data runnable through commandline arguments

--- news/get_news_rss.py
+++ news/get_news_rss.py
@@ -1,6 +1,10 @@
 import feedparser
 import pandas as pd
 from datetime import datetime
+from AuthFile import get_news_engine
+from bs4 import BeautifulSoup
+import re
+from io import StringIO
 
 raw_rss = ['https://news.google.com/rss?x=1571747254.2933&hl=en-US&gl=US&ceid=US:en'
             , 'https://www.reddit.com/r/worldnews/.rss'
@@ -32,11 +36,42 @@ for feed in feeds:
     for post in feed.entries:
         posts.append((post.title, post.link, post.summary))
 
+
 df = pd.DataFrame(posts, columns=['title', 'link', 'summary'])
+
+df.drop('link', axis=1)
 
 df['captured_at'] = str(datetime.now().strftime("%Y%m%d_%H:00"))
 
+# print(df)
+
+#
+# df['title'] = df['title'].apply(lambda x: re.split('https:\/\/.*', str(x))[0])
+# df['title'] = df['title'].apply(lambda x: re.split('^\s*$', str(x))[0])
+# df['title'] = df['title'].apply(lambda x: re.split('[`~!$%^&*()+={}|\[\]:";<>?,\./“”]*$', str(x))[0])
+#
+# df['summary'] = df['summary'].apply(lambda x: re.split('https:\/\/.*', str(x))[0])
+# df['summary'] = df['summary'].apply(lambda x: re.split('^\s*$', str(x))[0])
+# df['summary'] = df['summary'].apply(lambda x: re.split('[`~!$%^&*()+={}|\[\]:";<>?,\./“”]*$', str(x))[0])
+
+
+
+
+#
+# df['title'] = df['title'].str.encode('latin-1')
+#
+# df['summary'] = df['summary'].str.encode('latin-1')
+
+
+df['title'] = df['title'].astype('str')
+
+df['summary'] = df['summary'].astype('str')
+
 print(df)
+
+
+
+df.to_sql(schema='staging', name='news_now', con=get_news_engine(), if_exists='replace')
 
 # df.to_csv('news' + str(datetime.now().strftime("_%Y%m%d_%H00")+ '.csv'), index=False, mode='w')
 
