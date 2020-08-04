d9c1a10ae33c73c5aff75b0a40926b6473fd10b5
Julian Dragoi <julian_dragoi@hotmail.co.uk>
2020-08-05 00:07:45 +0100

    added function to metrics dag

--- maintenance/get_metrics.sql
+++ maintenance/get_metrics.sql
@@ -1,3 +1,18 @@
+create or replace function
+count_rows(schema text, tablename text) returns integer
+as
+$body$
+declare
+  result integer;
+  query varchar;
+begin
+  query := 'SELECT count(1) FROM ' || schema || '.' || tablename;
+  execute query into result;
+  return result;
+end;
+$body$
+language plpgsql;
+
 
 INSERT INTO core.table_metrics ("schema", table_name, row_count, size_mb)
 SELECT tbls.table_schema, tbls.table_name, tbls.rows_cnt,  pg_size_pretty(pg_total_relation_size(concat(tbls.table_schema,'.',tbls.table_name)))
