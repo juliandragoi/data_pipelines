54162dd86879a2744d539b34b93303ab066c1192
Julian Dragoi <julian_dragoi@hotmail.co.uk>
2020-11-09 17:58:36 +0000

    commented out updated dag for now

--- dags/scheduled.py
+++ dags/scheduled.py
@@ -21,47 +21,47 @@ main_dir = '/home/pi/data_pipelines'
 # daily server update
 # **********************
 
-update_servers_dag = DAG(
-    'update_servers',
-    schedule_interval="0 4 * * *",
-    catchup=False,
-    default_args=default_args
-)
-
-cmd = 'sudo apt-get dist-upgrade -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove -y'
-
-t1 = SSHOperator(
-    ssh_conn_id='pi1_ssh',
-    task_id='pi1_update',
-    command=cmd,
-    dag=update_servers_dag)
-
-
-t2 = BashOperator(
-    task_id='pi2_update',
-    bash_command=cmd,
-    dag=update_servers_dag)
-
-
-t3 = SSHOperator(
-    ssh_conn_id='pi3_ssh',
-    task_id='pi3_update',
-    command=cmd,
-    dag=update_servers_dag)
-
-
-t4 = SSHOperator(
-    ssh_conn_id='pi4_ssh',
-    task_id='pi4_update',
-    command=cmd,
-    dag=update_servers_dag)
-
-
-t5 = SSHOperator(
-    ssh_conn_id='pi5_ssh',
-    task_id='pi5_update',
-    command=cmd,
-    dag=update_servers_dag)
+# update_servers_dag = DAG(
+#     'update_servers',
+#     schedule_interval="0 4 * * *",
+#     catchup=False,
+#     default_args=default_args
+# )
+#
+# cmd = 'sudo apt-get dist-upgrade -y && sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove -y'
+#
+# t1 = SSHOperator(
+#     ssh_conn_id='pi1_ssh',
+#     task_id='pi1_update',
+#     command=cmd,
+#     dag=update_servers_dag)
+#
+#
+# t2 = BashOperator(
+#     task_id='pi2_update',
+#     bash_command=cmd,
+#     dag=update_servers_dag)
+#
+#
+# t3 = SSHOperator(
+#     ssh_conn_id='pi3_ssh',
+#     task_id='pi3_update',
+#     command=cmd,
+#     dag=update_servers_dag)
+#
+#
+# t4 = SSHOperator(
+#     ssh_conn_id='pi4_ssh',
+#     task_id='pi4_update',
+#     command=cmd,
+#     dag=update_servers_dag)
+#
+#
+# t5 = SSHOperator(
+#     ssh_conn_id='pi5_ssh',
+#     task_id='pi5_update',
+#     command=cmd,
+#     dag=update_servers_dag)
 
 
 # **********************
