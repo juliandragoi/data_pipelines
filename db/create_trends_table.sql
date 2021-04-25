723b3c01f2e864fec00012778bb549a2fea70b9e
Julian Dragoi <julian_dragoi@hotmail.co.uk>
2021-04-25 18:34:45 +0100

    removing files

--- db/create_trends_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS core.trends;
-
-CREATE TABLE core.trends(
-	id  			serial,
-	keyword 		varchar,
-	"source"		varchar,
-	captured_at 	timestamp DEFAULT now(),
-	CONSTRAINT trends_constraint UNIQUE (keyword)
-)
--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="utils",
-    version="0.0.1",
-    author="J_DAWG",
-    packages=setuptools.find_packages()
-)
