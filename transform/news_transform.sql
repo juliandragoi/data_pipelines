0178d2257103d39957de4de06d32b5f34dd81955
Julian Dragoi <julian_dragoi@hotmail.co.uk>
2021-05-15 01:18:30 +0100

    amended news_raw to news for core table

--- transform/news_transform.sql
+++ transform/news_transform.sql
@@ -1,4 +1,4 @@
-INSERT INTO core.news_raw  (source, title, summary, link, author, published, captured_at)
+INSERT INTO core.news  (source, title, summary, link, author, published, captured_at)
 SELECT source, title, summary, link, author, published, captured_at FROM staging.news_rss
 ON CONFLICT ON CONSTRAINT unique_news_constraint
 DO NOTHING;
